1737c61b87ec0924fb477fb57995475da50e3f5f
shah <info@shahnewazkhan.ca>
2023-05-18 16:49:20 -0700

    added prod sf api/udf integrations

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -12,7 +12,7 @@
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_json_rpc(
         json variant
     ) returns text api_integration = aws_terra_api AS {% if target.name == "prod" %}
-        '<REPLACE_WITH_PROD_URI>/udf_bulk_json_rpc'
+        'https://o94r0hypab.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_json_rpc'
     {% else %}
         'https://33fgv8p4d4.execute-api.us-east-1.amazonaws.com/sbx/udf_bulk_json_rpc'
     {%- endif %};
