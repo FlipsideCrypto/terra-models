bd5a30c872ac4a424165ebd427cc55252b738dd7
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-01-31 09:14:47 -0500

    labels

--- models/gold/core/core__dim_address_labels.sql
+++ models/gold/core/core__dim_address_labels.sql
@@ -9,25 +9,12 @@ SELECT
     creator,
     label_type,
     label_subtype,
-    label,
+    address_name AS label,
     project_name,
-    decimals,
-    deployment_tx_id,
-    COALESCE (
-        address_labels_id,
-        {{ dbt_utils.generate_surrogate_key(
-            ['address']
-        ) }}
-    ) AS dim_address_labels_id,
-    COALESCE(
-        inserted_timestamp,
-        '2000-01-01'
-    ) AS inserted_timestamp,
-    COALESCE(
-        modified_timestamp,
-        '2000-01-01'
-    ) AS modified_timestamp
+    NULL AS decimals,
+    NULL AS deployment_tx_id,
+    labels_combined_id AS dim_labels_id,
+    inserted_timestamp,
+    modified_timestamp
 FROM
     {{ ref('silver__address_labels') }}
-WHERE
-    address IS NOT NULL
--- models/silver/silver__address_labels.sql
+++ models/silver/silver__address_labels.sql
@@ -1,101 +1,41 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = 'address',
+    unique_key = ['address', 'blockchain'],
+    incremental_strategy = 'merge',
     merge_exclude_columns = ["inserted_timestamp"],
+    cluster_by = 'modified_timestamp::DATE',
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(address); DELETE FROM {{ this }} WHERE _is_deleted = TRUE;",
     tags = ['noncore']
 ) }}
 
-WITH labels AS (
-
-    SELECT
-        blockchain,
-        address,
-        creator,
-        label_type,
-        label_subtype,
-        address_name AS label,
-        project_name,
-        system_created_at AS _inserted_timestamp
-    FROM
-        {{ source(
-            'crosschain_silver',
-            'address_labels'
-        ) }}
-    WHERE
-        blockchain = 'terra'
-        AND delete_flag IS NULL
-        AND {{ incremental_load_filter('_inserted_timestamp') }}
-        qualify ROW_NUMBER() over (
-            PARTITION BY address
-            ORDER BY
-                creator DESC
-        ) = 1
-),
-tokens AS (
-    SELECT
-        blockchain,
-        'token_deployment' AS creator,
-        tx_id,
-        label,
-        symbol,
-        address,
-        decimals,
-        _inserted_timestamp
-    FROM
-        {{ ref('silver__token_labels') }}
-),
-FINAL AS (
-    SELECT
-        COALESCE(
-            t.blockchain,
-            l.blockchain
-        ) AS blockchain,
-        COALESCE(
-            t.address,
-            l.address
-        ) AS address,
-        COALESCE(
-            t.creator,
-            l.creator
-        ) AS creator,
-        IFF(
-            l.label_type IS NOT NULL,
-            l.label_type,
-            'token'
-        ) AS label_type,
-        IFF(
-            l.label_subtype IS NOT NULL,
-            l.label_subtype,
-            'token_contract'
-        ) AS label_subtype,
-        COALESCE(
-            t.symbol,
-            l.label
-        ) AS label,
-        COALESCE(
-            t.label,
-            l.project_name
-        ) AS project_name,
-        t.decimals,
-        t.tx_id AS deployment_tx_id,
-        GREATEST(
-            l._inserted_timestamp,
-            t._inserted_timestamp
-        ) AS _inserted_timestamp,
-        {{ dbt_utils.generate_surrogate_key(
-            ['COALESCE( t.address, l.address )']
-        ) }} AS address_labels_id,
-        SYSDATE() AS inserted_timestamp,
-        SYSDATE() AS modified_timestamp,
-        '{{ invocation_id }}' AS _invocation_id
-    FROM
-        labels l full
-        JOIN tokens t USING (
-            blockchain,
-            address
-        )
-)
 SELECT
-    *
+    system_created_at,
+    insert_date,
+    blockchain,
+    address,
+    creator,
+    label_type,
+    label_subtype,
+    address_name,
+    project_name,
+    _is_deleted,
+    labels_combined_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
 FROM
-    FINAL
+    {{ source(
+        'crosschain_silver',
+        'labels_combined'
+    ) }}
+WHERE
+    blockchain = 'terra'
+
+{% if is_incremental() %}
+AND modified_timestamp >= (
+    SELECT
+        MAX(modified_timestamp)
+    FROM
+        {{ this }}
+)
+{% endif %}
